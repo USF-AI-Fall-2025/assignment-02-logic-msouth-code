3c99a2d6ca33c9b3194607ef4a469b6eb36a7e1e
Miaya South <81059116+msouth-code@users.noreply.github.com>
2025-10-06 22:31:35 -0700

    Add files via upload

--- DataInvestigator.py
+++ DataInvestigator.py
@@ -1,11 +1,38 @@
+
 class DataInvestigator:
 
+    def __init__(self, df):
+        self.df = df
+
     def baseline(self, col):
-        pass
+        column_values = self.df.iloc[:, col].values
+        return max(set(column_values.tolist()), key=column_values.tolist().count)
+        
 
     def corr(self, col1, col2):
-        pass
+        correlation = self.df.iloc[:, col1].corr(self.df.iloc[:, col2])
+        return correlation
 
     def zeroR(self, col):
-        pass
+        column_values = self.df.iloc[:, col].values
+        most_common = max(set(column_values.tolist()), key=column_values.tolist().count)
+        return most_common
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('gallstone.csv')
+di = DataInvestigator(df)
+
+sns.displot(data=df, x='Glucose', kind='hist', kde=True)
+plt.show()
+
+print(df.groupby("Gender")[["Height","Weight","Body Mass Index (BMI)",
+                            "Total Body Fat Ratio (TBFR) (%)",
+                            "Hemoglobin (HGB)"]].mean())
+print(df.groupby("Gender")["Hemoglobin (HGB)"].describe())
+print(df.groupby("Gender")["Total Body Fat Ratio (TBFR) (%)"].describe())
+
+
+print(di.baseline(1))
